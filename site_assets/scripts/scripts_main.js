8b2ebcd90ced9d5eddeed039d2d090f6a70d7f81
Timothy Wang <2556936w@student.gla.ac.uk>
2022-12-05 14:30:45 +0000

    Initial site setup

---
+++ site_assets/scripts/scripts_main.js
@@ -0,0 +1,27 @@
+/* Global scripts */
+
+$(document).ready(function(){
+
+	// Animate Scrolling on link click
+	$('a[href*="#"]').on('click', function (e) {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 600, 'swing');
+	});
+
+	// Back to top button
+	var btn = $('#back2top');
+	$(window).scroll(function() {
+	  if ($(window).scrollTop() > 300) {
+		btn.addClass('show');
+	  } else {
+		btn.removeClass('show');
+	  }
+	});
+	btn.on('click', function(e) {
+	  e.preventDefault();
+	  $('html, body').animate({scrollTop:0}, '300');
+	});
+
+
+});
